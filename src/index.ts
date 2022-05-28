add64188e42322ba754e66c367bd5d071c1b1e4c
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-28 20:05:21 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@
 */
 
 
-// declaro una Funciones para solicitar la cantidad de alumnos (hasta 10 alumnos, es la versión no paga jajja) y validar las nota
-function noteStudent (position:number):number
+// declaro una Funciones para solicitar la cantidad de alumnos (hasta 10 alumnos, es la versión no paga jajja) 
+// Tambien valido las nota por cada alumno, la position and name es solo para ponerlo en el prompt 
+function noteStudent (position:number , Name:string):number
   {
     let itsnotnum: boolean = true; // lo pongo en true para obligar a entre en el chequeo 
     let num: number=0;
@@ -14,9 +15,9 @@ function noteStudent (position:number):number
        if (position<0) {
           num = Number(prompt("Ingrese la cantidad de Alumnos: "));
           } else {
-              num = Number(prompt("Ingrese la nota del Examen (de 0 a 10) " + position));
+              num = Number(prompt("Ingrese la Nota " + position + " del Alumno " + Name + " (de 0 a 10) " ));
               }
-      if (isNaN(num) || num % 1 || num<=10){
+      if (isNaN(num) || num % 1 || num>10){
                 alert("Solos se adminten un números entero entre el rango 0 y 10");
                 } else {
                   itsnotnum = false;
@@ -25,37 +26,31 @@ function noteStudent (position:number):number
       }
   }
 
-  function nameStudent (countnames:number):string[]
-  {
-    let names: string[] = nuw Array (countnames);
-    for (let i:number=0;i<countnames;i++)
-      
-    return names;
-  }
-  //Empiezo con mi programa main
+//Empiezo con mi programa main
 console.clear();
-let longvector: number = noteStudent(-1);
-let countstudent:number[] = new Array (longvector);
-let note1:number[]= new Array (longvector);
-let note2:number[]= new Array (longvector);
-let note3:number[]= new Array (longvector);
+let countstudent: number = noteStudent(-1,"I dunno");
+let NamesStudents: string[] = new Array (countstudent);
+let note1:number[]= new Array (countstudent); //Las creo para mantener las notas 
+let note2:number[]= new Array (countstudent); //Las creo para mantener las notas 
+let note3:number[]= new Array (countstudent); //Las creo para mantener las notas 
+let averagenote: number[]= new Array (countstudent);
 
-
-
-for (let i: number = 0; i <longvector; i++)                                //Cargo los datos y comparo
+//ingreso todos los nombres
+for (let i:number=0; i<countstudent ; i++)
   {
-    vector[i]=isnumber(i+1);
-    if (vector[i]===0) 
-    {
-      countzero++;
-    } else {
-            if (vector[i]>0)
-              {
-               countpos++;   
-              } else {
-                      countneg++;
-                      }
+   NamesStudents[i] = String (prompt("Ingrese el nombre del alumno " + (i+1)));
+  } 
 
-            }
-  }
-console.log("Hay: " + countpos + " números positivos, " + countneg + " números negativos y " + countzero + " Ceros");
+//Pido las notas de cada alumno llamando a la función con sus nombres
+for (let i:number=0;i<countstudent;i++)
+{
+ note1[i]= noteStudent(1,NamesStudents[i]); 
+ note2[i]= noteStudent(2,NamesStudents[i]);
+ note3[i]= noteStudent(3,NamesStudents[i]);
+ averagenote[i]= (note1[i]+note2[i]+note3[i])/3;
+}
+
+for (let i:number=0;i<countstudent;i++)
+{
+  console.log("El promedio de " + NamesStudents[i] + " es: "+ averagenote[i].toPrecision(2));
+}
